7e81c129f9b07d9b0d7ec468ba212d184819d30b
RamilMusazada <themusazadeh@gmail.com>
2025-05-08 14:09:52 +0400

    correct implementation in User entity to return actual username

--- src/main/java/org/example/socialmediabackend/dto/LoginUserDto.java
+++ src/main/java/org/example/socialmediabackend/dto/LoginUserDto.java
@@ -1,11 +1,17 @@
 package org.example.socialmediabackend.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class LoginUserDto {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Please provide a valid email address")
     private String email;
+
+    @NotBlank(message = "Password is required")
     private String password;
 }
